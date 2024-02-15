fee37d245c5c196a44eab3136741091dfacb7fa4
Shubham Daule <sbdaule5@gmail.com>
2024-02-16 03:19:22 +0530

    Make an ad-hoc hero section for deployment

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -5,17 +5,34 @@
 </svelte:head>
 
 <div class="hero" style="font-size: 2rem;">
+	<div class="typo">
+		<img src="/assets/hero_typo.png" height="100%" alt="MERAZ"/>
+	</div>
 </div>
 
 <style lang="scss">
 	.hero {
+		position: relative;
 		width: 100%;
 		height: 100vh;
-		background-image: url("assets/hero_bg.png");
+		background-image: url("/assets/hero_bg.png");
 		background-size: cover;
 		background-position: center;
 		position: relative;
 		text-align: center;
 		scroll-snap-align: start;
+	}
+	.typo{
+		position: absolute;
+		height: 60vh;
+		width: 95vw;
+		top:50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		img{
+			height: 100%;
+			width: 100%;
+			object-fit: contain;
+		}
 	}
 </style>
